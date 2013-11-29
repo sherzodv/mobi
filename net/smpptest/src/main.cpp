22b4d935a41643d909c01510825b4d4f4aac9f68
oybek <aybek_hashimov@mail.ru>
2013-11-30 00:48:27 +0600

    Corrected boost test
    	boost test

--- net/smpptest/src/main.cpp
+++ net/smpptest/src/main.cpp
@@ -5,8 +5,6 @@
 #include <cstdio>
 
 namespace local {
-	using namespace smpp;
-
 	const int buf_size			= 0x1000;
 	const std::string file_name	= "/tmp/.buffer";
 
@@ -56,107 +54,114 @@ namespace local {
 		ITS_REPLY_TYPE				= 0x1380,
 		ITS_SESSION_INFO			= 0x1383
 	};
-
-	BOOST_AUTO_TEST_CASE( test_tlv_1octet_val )
-	{
-		proto::u8_t * buf = new proto::u8_t [buf_size];
-
-		tlv_dest_addr_subunit ob1, ob2;
-
-		ob1.tag = DEST_NUM_SUBUNIT;
-		ob1.len = 0x0001;
-		ob1.val = 0x0F;
-
-		write(buf, ob1, std::cout);
-		parse(ob2, buf, std::cout);
-
-		BOOST_CHECK(ob1.tag == ob2.tag);
-		BOOST_CHECK(ob1.len == ob2.len);
-		BOOST_CHECK(ob1.val == ob2.val);
-	}
-
-	BOOST_AUTO_TEST_CASE( test_tlv_2octet_val )
-	{
-		proto::u8_t * buf = new proto::u8_t [buf_size];
-
-		tlv_dest_telematics_id ob1, ob2;
-
-		ob1.tag = DEST_TELEMATICS_ID;
-		ob1.len = 0x0002;
-		ob1.val = 0x0100;
-
-		write(buf, ob1, std::cout);
-		parse(ob2, buf, std::cout);
-
-		BOOST_CHECK(ob1.tag == ob2.tag);
-		BOOST_CHECK(ob1.len == ob2.len);
-		BOOST_CHECK(ob1.val == ob2.val);
-	}
-
-	BOOST_AUTO_TEST_CASE( test_tlv_4octet_val )
-	{
-		proto::u8_t * buf = new proto::u8_t [buf_size];
-
-		tlv_qos_time_to_live ob1, ob2;
-
-		ob1.tag = QOS_TIME_TO_LIVE;
-		ob1.len = 0x0004;
-		ob1.val = 0x00000100;
-
-		write(buf, ob1, std::cout);
-		parse(ob2, buf, std::cout);
-
-		BOOST_CHECK(ob1.tag == ob2.tag);
-		BOOST_CHECK(ob1.len == ob2.len);
-		BOOST_CHECK(ob1.val == ob2.val);
-	}
-
-	BOOST_AUTO_TEST_CASE( test_tlv_all_val )
-	{
-		proto::u8_t * buf = new proto::u8_t [buf_size];
-
-		tlv_dest_addr_subunit ob8_1, ob8_2;
-		tlv_dest_telematics_id ob16_1, ob16_2;
-		tlv_qos_time_to_live ob32_1, ob32_2;
-
-		ob8_1.tag = DEST_NUM_SUBUNIT;
-		ob8_1.len = 0x0001;
-		ob8_1.val = 0x0F;
-
-		ob16_1.tag = DEST_TELEMATICS_ID;
-		ob16_1.len = 0x0002;
-		ob16_1.val = 0x0100;
-
-		ob32_1.tag = QOS_TIME_TO_LIVE;
-		ob32_1.len = 0x0004;
-		ob32_1.val = 0x00000100;
-
-		{
-			proto::u8_t * curr = buf;
-			curr = write(curr, ob8_1, std::cout);
-			curr = write(curr, ob16_1, std::cout);
-			curr = write(curr, ob32_1, std::cout);
-		}
-
-		{
-			const proto::u8_t * curr = buf;
-			curr = parse(ob8_2, curr, std::cout);
-			curr = parse(ob16_2, curr, std::cout);
-			curr = parse(ob32_2, curr, std::cout);
-		}
-
-		BOOST_CHECK(ob8_1.tag == ob8_2.tag);
-		BOOST_CHECK(ob8_1.len == ob8_2.len);
-		BOOST_CHECK(ob8_1.val == ob8_2.val);
-
-		BOOST_CHECK(ob16_1.tag == ob16_2.tag);
-		BOOST_CHECK(ob16_1.len == ob16_2.len);
-		BOOST_CHECK(ob16_1.val == ob16_2.val);
-
-		BOOST_CHECK(ob32_1.tag == ob32_2.tag);
-		BOOST_CHECK(ob32_1.len == ob32_2.len);
-		BOOST_CHECK(ob32_1.val == ob32_2.val);
-	}
 }
 
+BOOST_AUTO_TEST_CASE( test_tlv_1octet_val )
+{
+	using namespace smpp;
+	using namespace local;
+	proto::u8_t * buf = new proto::u8_t [buf_size];
+
+	tlv_dest_addr_subunit ob1, ob2;
+
+	ob1.tag = DEST_NUM_SUBUNIT;
+	ob1.len = 0x0001;
+	ob1.val = 0x0F;
+
+	write(buf, ob1, std::cout);
+	parse(ob2, buf, std::cout);
+
+	BOOST_CHECK(ob1.tag == ob2.tag);
+	BOOST_CHECK(ob1.len == ob2.len);
+	BOOST_CHECK(ob1.val == ob2.val);
+}
+
+BOOST_AUTO_TEST_CASE( test_tlv_2octet_val )
+{
+	using namespace smpp;
+	using namespace local;
+	proto::u8_t * buf = new proto::u8_t [buf_size];
+
+	tlv_dest_telematics_id ob1, ob2;
+
+	ob1.tag = DEST_TELEMATICS_ID;
+	ob1.len = 0x0002;
+	ob1.val = 0x0100;
+
+	write(buf, ob1, std::cout);
+	parse(ob2, buf, std::cout);
+
+	BOOST_CHECK(ob1.tag == ob2.tag);
+	BOOST_CHECK(ob1.len == ob2.len);
+	BOOST_CHECK(ob1.val == ob2.val);
+}
+
+BOOST_AUTO_TEST_CASE( test_tlv_4octet_val )
+{
+	using namespace smpp;
+	using namespace local;
+	proto::u8_t * buf = new proto::u8_t [buf_size];
+
+	tlv_qos_time_to_live ob1, ob2;
+
+	ob1.tag = QOS_TIME_TO_LIVE;
+	ob1.len = 0x0004;
+	ob1.val = 0x00000100;
+
+	write(buf, ob1, std::cout);
+	parse(ob2, buf, std::cout);
+
+	BOOST_CHECK(ob1.tag == ob2.tag);
+	BOOST_CHECK(ob1.len == ob2.len);
+	BOOST_CHECK(ob1.val == ob2.val);
+}
+
+BOOST_AUTO_TEST_CASE( test_tlv_all_val )
+{
+	using namespace smpp;
+	using namespace local;
+	proto::u8_t * buf = new proto::u8_t [buf_size];
+
+	tlv_dest_addr_subunit ob8_1, ob8_2;
+	tlv_dest_telematics_id ob16_1, ob16_2;
+	tlv_qos_time_to_live ob32_1, ob32_2;
+
+	ob8_1.tag = DEST_NUM_SUBUNIT;
+	ob8_1.len = 0x0001;
+	ob8_1.val = 0x0F;
+
+	ob16_1.tag = DEST_TELEMATICS_ID;
+	ob16_1.len = 0x0002;
+	ob16_1.val = 0x0100;
+
+	ob32_1.tag = QOS_TIME_TO_LIVE;
+	ob32_1.len = 0x0004;
+	ob32_1.val = 0x00000100;
+
+	{
+		proto::u8_t * curr = buf;
+		curr = write(curr, ob8_1, std::cout);
+		curr = write(curr, ob16_1, std::cout);
+		curr = write(curr, ob32_1, std::cout);
+	}
+
+	{
+		const proto::u8_t * curr = buf;
+		curr = parse(ob8_2, curr, std::cout);
+		curr = parse(ob16_2, curr, std::cout);
+		curr = parse(ob32_2, curr, std::cout);
+	}
+
+	BOOST_CHECK(ob8_1.tag == ob8_2.tag);
+	BOOST_CHECK(ob8_1.len == ob8_2.len);
+	BOOST_CHECK(ob8_1.val == ob8_2.val);
+
+	BOOST_CHECK(ob16_1.tag == ob16_2.tag);
+	BOOST_CHECK(ob16_1.len == ob16_2.len);
+	BOOST_CHECK(ob16_1.val == ob16_2.val);
+
+	BOOST_CHECK(ob32_1.tag == ob32_2.tag);
+	BOOST_CHECK(ob32_1.len == ob32_2.len);
+	BOOST_CHECK(ob32_1.val == ob32_2.val);
+}
 
