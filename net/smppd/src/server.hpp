02e71f1dbe074117bc2f6ce20d282ef162da79da
Sherzod Gapirov <sherzod.gapirov@mail.ru>
2014-01-09 09:46:54 +0600

    smppd: Slight code refactoring.

--- net/smppd/src/server.hpp
+++ net/smppd/src/server.hpp
@@ -35,6 +35,24 @@ class server: public service<ProtoT, AllocatorT, LogT> {
 
 		virtual bool authenticate(const std::string & sys_id, const std::string & password) = 0;
 
+		void on_send(bin::sz_t channel_id, bin::sz_t msg_id) {
+			(void)(channel_id);
+			(void)(msg_id);
+		}
+
+		void on_send_error(bin::sz_t channel_id, bin::sz_t msg_id) {
+			(void)(channel_id);
+			(void)(msg_id);
+		}
+
+		void on_recv_error(bin::sz_t channel_id) {
+			(void)(channel_id);
+		}
+
+		void on_parse_error(bin::sz_t channel_id) {
+			(void)(channel_id);
+		}
+
 		void on_bind_transmitter(bin::sz_t channel_id, const bind_transmitter & msg) {
 			ltrace(L) << "channel#" << channel_id << " got: " << msg;
 		}
@@ -60,10 +78,6 @@ class server: public service<ProtoT, AllocatorT, LogT> {
 			(void)(channel_id);(void)(msg);
 		}
 
-		void on_unbind_r(bin::sz_t channel_id, const unbind_r & msg) {
-			(void)(channel_id);(void)(msg);
-		}
-
 		void on_outbind(bin::sz_t channel_id, const outbind & msg) {
 			(void)(channel_id);(void)(msg);
 		}
@@ -76,43 +90,24 @@ class server: public service<ProtoT, AllocatorT, LogT> {
 			ltrace(L) << "channel#" << channel_id << " got: " << msg;
 		}
 
-		void on_send(bin::sz_t channel_id, bin::sz_t msg_id) {
-			(void)(channel_id);
-			(void)(msg_id);
-		}
-
-		void on_send_error(bin::sz_t channel_id, bin::sz_t msg_id) {
-			(void)(channel_id);
-			(void)(msg_id);
-		}
-
-		void on_recv_error(bin::sz_t channel_id) {
-			(void)(channel_id);
-		}
-
-		void on_parse_error(bin::sz_t channel_id) {
-			(void)(channel_id);
-		}
-
-		/* Bind responses are not expected at server side */
+		/* Messages which are not expected at the server side */
 		void on_bind_transmitter_r(bin::sz_t channel_id, const bind_transmitter_r & msg) {
-			lerror(L) << channel_id << "got bind_transmitter_r";
-			lerror(L) << channel_id << msg;
+			lerror(L) << channel_id << "got bind_transmitter_r: " << msg;
 		}
 		void on_bind_receiver_r(bin::sz_t channel_id, const bind_receiver_r & msg) {
-			lerror(L) << channel_id << "got bind_receiver_r";
-			ltrace(L) << channel_id << msg;
+			lerror(L) << channel_id << "got bind_receiver_r: " << msg;
 		}
 		void on_bind_transceiver_r(bin::sz_t channel_id, const bind_transceiver_r & msg) {
-			lerror(L) << channel_id << "got bind_transceiver_r";
-			ltrace(L) << channel_id << msg;
+			lerror(L) << channel_id << "got bind_transceiver_r: " << msg;
 		}
 		void on_submit_sm_r(bin::sz_t channel_id, const submit_sm_r & msg) {
 			(void)(msg);
 			lerror(L) << channel_id << "got submit_sm_r";
 			//ltrace(L) << channel_id << msg;
 		}
-
+		void on_unbind_r(bin::sz_t channel_id, const unbind_r & msg) {
+			lerror(L) << channel_id << "got unbind_r: " << msg;
+		}
 };
 
 template <class AllocatorT, class LogT>
